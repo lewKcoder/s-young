f837701942dfb7a15150f27a4a881d9a03e05253
lewKcoder <7kamimura74@gmail.com>
2023-07-29 14:49:42 +0900

    Added site-title

---
+++ src/components/site-title/site-title.tsx
@@ -0,0 +1,6 @@
+import { FunctionComponent } from 'react';
+import styles from './styles.module.scss';
+
+export const SiteTitle: FunctionComponent = () => {
+  return <h1 className={styles.container}>未来を生きる主役は国を変える。</h1>;
+};
--- src/layouts/home/home.tsx
+++ src/layouts/home/home.tsx
@@ -1,11 +1,13 @@
 import { FunctionComponent } from 'react';
 import { Header } from '@/components/header';
+import { SiteTitle } from '@/components/site-title';
 import { Introduction } from '@/components/introduction';
 
 export const Home: FunctionComponent = () => {
   return (
     <>
       <Header />
+      <SiteTitle />
       <Introduction />
     </>
   );
