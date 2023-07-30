95cbb62b3f3ddf09bac81ed1fbd56e357f37f8fd
lewKcoder <7kamimura74@gmail.com>
2023-07-30 11:55:36 +0900

    Added GSAP animation

--- src/components/vote/vote.tsx
+++ src/components/vote/vote.tsx
@@ -1,20 +1,69 @@
-import { FunctionComponent } from 'react';
+import { FunctionComponent, useEffect } from 'react';
 import styles from './styles.module.scss';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
 
 export const Vote: FunctionComponent = () => {
+  const timeline = gsap.timeline({
+    scrollTrigger: {
+      trigger: '#RedCard',
+      start: '+120px center',
+      end: '+1000px top',
+      scrub: true,
+    },
+  });
+
+  useEffect(() => {
+    timeline
+      .to('#RedCard', {
+        opacity: 0.5,
+        top: 'calc(50% - -400px)',
+        borderColor: '#ff5454',
+      })
+      .to('#RedCard', {
+        borderRadius: '50%',
+        scale: 0,
+        width: 10,
+        height: 10,
+        top: 'calc(50% - -450px)',
+        left: 'calc(50% - 37px)',
+      })
+      .to('#RedCard', {
+        scale: 50,
+        opacity: 0,
+        display: 'none',
+      });
+
+    gsap.to('#WhiteCard', {
+      top: 'calc(50% - -923px)',
+      ease: 'none',
+      scrollTrigger: {
+        trigger: '#WhiteCard',
+        scrub: true,
+        start: '+115px center',
+        end: '+40px top',
+      },
+    });
+  }, []);
+
   return (
     <div className={styles.container}>
       <img src="/vote-hand.svg" alt="Vote" className={styles.vote} />
 
-      <img src="/red-card.svg" alt="RedCard" className={styles.red_card} />
+      <div id="RedCard" className={styles.red_card} />
 
       <div className={styles.white_card}>
-        <img src="/white-card.svg" alt="WhiteCard" className={styles.image} />
+        <img
+          id="WhiteCard"
+          src="/white-card.svg"
+          alt="WhiteCard"
+          className={styles.white_card_image}
+        />
       </div>
 
       <div className={styles.bar} />
-
-      <div className={styles.circle} />
     </div>
   );
 };
