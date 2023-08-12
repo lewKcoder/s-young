0176f833294f35f3fb14d175aa505a8f2765c036
lewKcoder <7kamimura74@gmail.com>
2023-08-12 11:55:29 +0900

    Fixed styles and prohibition

--- src/components/thread-item/thread-item.tsx
+++ src/components/thread-item/thread-item.tsx
@@ -4,34 +4,73 @@ import { ThreadSkeltons } from '@/components/thread-skeltons';
 import styles from './styles.module.scss';
 import { API, graphqlOperation } from 'aws-amplify';
 import { listChats } from '@/graphql/queries';
+import { createProhibition } from '@/graphql/mutations';
+
+type Chat = {
+  id: string;
+  userId: string;
+  chatId: string;
+  userName: string;
+  iconColor: string;
+  text: string;
+  date: string;
+  likes: number;
+  prohibition: boolean;
+}[];
 
 export const ThreadItem: FunctionComponent = () => {
-  const [chats, setChat] = useState([]);
+  const [chats, setChat] = useState<Chat | []>([]);
 
   useEffect(() => {
     getChats();
   }, []);
 
-  async function getChats() {
+  const getChats = async () => {
     try {
       const chatData = await API.graphql(graphqlOperation(listChats));
 
       if ('data' in chatData) {
-        console.log(chatData.data.listChats.items);
         setChat(chatData.data.listChats.items);
       }
     } catch (err) {
       console.log('error fetching chat:', err);
     }
-  }
+  };
+
+  const sendProhibition = async (id: string, userName: string, date: string, text: string) => {
+    const param = {
+      input: {
+        id: id,
+        userName: userName,
+        text: text,
+        date: new Date(date).toISOString(),
+        report: 'テスト',
+      },
+    };
+
+    try {
+      await API.graphql(graphqlOperation(createProhibition, param));
+    } catch (err) {
+      console.log('error createProhibition:', err);
+    }
+  };
 
   return (
     <div className={`${styles.container} ${chats.length === 0 && styles.has_loading}`}>
       {chats.length > 0 ? (
         <ul className={styles.content}>
-          {chats.map(({ id, date, text, userName, likes, iconColor }) => (
+          {chats.map(({ id, iconColor, userName, date, text, likes }) => (
             <li key={id} className={styles.list}>
-              <div className={styles.user_icon} style={{ background: `${iconColor}` }}>
+              <div
+                className={styles.user_icon}
+                style={{
+                  background: `${
+                    iconColor.length > 0
+                      ? iconColor
+                      : 'linear-gradient(40deg, rgb(23, 51, 2), rgb(95 95 95), rgb(17, 38, 70))'
+                  }`,
+                }}
+              >
                 <span className={styles.vote}>
                   <Image src="/vote-white.svg" alt="voteWhite" />
                 </span>
@@ -42,9 +81,13 @@ export const ThreadItem: FunctionComponent = () => {
                 <p className={styles.comment}>{text}</p>
               </div>
               <div className={styles.action_content}>
-                <span className={styles.icon}>
+                <span
+                  className={styles.icon}
+                  onClick={() => sendProhibition(id, userName, date, text)}
+                >
                   <Image src="/prohibition.png" alt="Prohibition" />
                 </span>
+
                 <div className={styles.like}>
                   {likes && <span>{likes}</span>}
                   <span className={styles.icon}>
