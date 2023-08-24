cb98390e4838d57b0335fa426f86f88f08cf79e0
lewKcoder <7kamimura74@gmail.com>
2023-08-24 18:43:57 +0900

    Added chat-api type and sort

--- src/data/get-chats-api/get-chats-api.tsx
+++ src/data/get-chats-api/get-chats-api.tsx
@@ -1,5 +1,6 @@
 import { API, graphqlOperation } from 'aws-amplify';
 import { listChats } from '@/graphql/queries';
+import { Chat } from './types';
 
 export const GetChatsAPI = () => {
   const getChats = async () => {
@@ -7,7 +8,19 @@ export const GetChatsAPI = () => {
       const chatData = await API.graphql(graphqlOperation(listChats));
 
       if ('data' in chatData) {
-        return chatData.data.listChats.items;
+        const chats = chatData.data.listChats.items;
+
+        chats.sort((a: Chat, b: Chat) => {
+          const dateA = new Date(a.date);
+          const dateB = new Date(b.date);
+          return dateA.getTime() - dateB.getTime();
+        });
+
+        if (chats.length === 0) {
+          return [];
+        }
+
+        return chats;
       }
     } catch (err) {
       console.log('error fetching chat:', err);
