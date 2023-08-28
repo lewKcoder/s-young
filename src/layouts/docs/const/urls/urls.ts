13b3f2149dedd7dcd11442a8007c81f81ce38223
lewKcoder <7kamimura74@gmail.com>
2023-08-29 00:40:21 +0900

    Added docs index

---
+++ src/layouts/docs/const/urls/urls.ts
@@ -0,0 +1,852 @@
+export const indexData = [
+  {
+    indexText: 'あ',
+    lists: [
+      {
+        title: '衆議院',
+        href: '#',
+      },
+      {
+        title: '参議院',
+        href: '#',
+      },
+      {
+        title: '首相',
+        href: '#',
+      },
+      {
+        title: '内閣',
+        href: '#',
+      },
+      {
+        title: '憲法',
+        href: '#',
+      },
+      {
+        title: '自民党',
+        href: '#',
+      },
+      {
+        title: '野党',
+        href: '#',
+      },
+      {
+        title: '総選挙',
+        href: '#',
+      },
+      {
+        title: '議員',
+        href: '#',
+      },
+      {
+        title: '政策',
+        href: '#',
+      },
+      {
+        title: '省庁',
+        href: '#',
+      },
+      {
+        title: '地方自治体',
+        href: '#',
+      },
+      {
+        title: '政党',
+        href: '#',
+      },
+      {
+        title: '政治家',
+        href: '#',
+      },
+      {
+        title: '政治献金',
+        href: '#',
+      },
+      {
+        title: '改革',
+        href: '#',
+      },
+      {
+        title: '民主党',
+        href: '#',
+      },
+      {
+        title: '税制',
+        href: '#',
+      },
+      {
+        title: '安全保障',
+        href: '#',
+      },
+      {
+        title: '外交',
+        href: '#',
+      },
+    ],
+  },
+  {
+    indexText: 'か',
+    lists: [
+      {
+        title: '衆議院',
+        href: '#',
+      },
+      {
+        title: '参議院',
+        href: '#',
+      },
+      {
+        title: '首相',
+        href: '#',
+      },
+      {
+        title: '内閣',
+        href: '#',
+      },
+      {
+        title: '憲法',
+        href: '#',
+      },
+      {
+        title: '自民党',
+        href: '#',
+      },
+      {
+        title: '野党',
+        href: '#',
+      },
+      {
+        title: '総選挙',
+        href: '#',
+      },
+      {
+        title: '議員',
+        href: '#',
+      },
+      {
+        title: '政策',
+        href: '#',
+      },
+      {
+        title: '省庁',
+        href: '#',
+      },
+      {
+        title: '地方自治体',
+        href: '#',
+      },
+      {
+        title: '政党',
+        href: '#',
+      },
+      {
+        title: '政治家',
+        href: '#',
+      },
+      {
+        title: '政治献金',
+        href: '#',
+      },
+      {
+        title: '改革',
+        href: '#',
+      },
+      {
+        title: '民主党',
+        href: '#',
+      },
+      {
+        title: '税制',
+        href: '#',
+      },
+      {
+        title: '安全保障',
+        href: '#',
+      },
+      {
+        title: '外交',
+        href: '#',
+      },
+    ],
+  },
+  {
+    indexText: 'さ',
+    lists: [
+      {
+        title: '衆議院',
+        href: '#',
+      },
+      {
+        title: '参議院',
+        href: '#',
+      },
+      {
+        title: '首相',
+        href: '#',
+      },
+      {
+        title: '内閣',
+        href: '#',
+      },
+      {
+        title: '憲法',
+        href: '#',
+      },
+      {
+        title: '自民党',
+        href: '#',
+      },
+      {
+        title: '野党',
+        href: '#',
+      },
+      {
+        title: '総選挙',
+        href: '#',
+      },
+      {
+        title: '議員',
+        href: '#',
+      },
+      {
+        title: '政策',
+        href: '#',
+      },
+      {
+        title: '省庁',
+        href: '#',
+      },
+      {
+        title: '地方自治体',
+        href: '#',
+      },
+      {
+        title: '政党',
+        href: '#',
+      },
+      {
+        title: '政治家',
+        href: '#',
+      },
+      {
+        title: '政治献金',
+        href: '#',
+      },
+      {
+        title: '改革',
+        href: '#',
+      },
+      {
+        title: '民主党',
+        href: '#',
+      },
+      {
+        title: '税制',
+        href: '#',
+      },
+      {
+        title: '安全保障',
+        href: '#',
+      },
+      {
+        title: '外交',
+        href: '#',
+      },
+    ],
+  },
+  {
+    indexText: 'た',
+    lists: [
+      {
+        title: '衆議院',
+        href: '#',
+      },
+      {
+        title: '参議院',
+        href: '#',
+      },
+      {
+        title: '首相',
+        href: '#',
+      },
+      {
+        title: '内閣',
+        href: '#',
+      },
+      {
+        title: '憲法',
+        href: '#',
+      },
+      {
+        title: '自民党',
+        href: '#',
+      },
+      {
+        title: '野党',
+        href: '#',
+      },
+      {
+        title: '総選挙',
+        href: '#',
+      },
+      {
+        title: '議員',
+        href: '#',
+      },
+      {
+        title: '政策',
+        href: '#',
+      },
+      {
+        title: '省庁',
+        href: '#',
+      },
+      {
+        title: '地方自治体',
+        href: '#',
+      },
+      {
+        title: '政党',
+        href: '#',
+      },
+      {
+        title: '政治家',
+        href: '#',
+      },
+      {
+        title: '政治献金',
+        href: '#',
+      },
+      {
+        title: '改革',
+        href: '#',
+      },
+      {
+        title: '民主党',
+        href: '#',
+      },
+      {
+        title: '税制',
+        href: '#',
+      },
+      {
+        title: '安全保障',
+        href: '#',
+      },
+      {
+        title: '外交',
+        href: '#',
+      },
+    ],
+  },
+  {
+    indexText: 'な',
+    lists: [
+      {
+        title: '衆議院',
+        href: '#',
+      },
+      {
+        title: '参議院',
+        href: '#',
+      },
+      {
+        title: '首相',
+        href: '#',
+      },
+      {
+        title: '内閣',
+        href: '#',
+      },
+      {
+        title: '憲法',
+        href: '#',
+      },
+      {
+        title: '自民党',
+        href: '#',
+      },
+      {
+        title: '野党',
+        href: '#',
+      },
+      {
+        title: '総選挙',
+        href: '#',
+      },
+      {
+        title: '議員',
+        href: '#',
+      },
+      {
+        title: '政策',
+        href: '#',
+      },
+      {
+        title: '省庁',
+        href: '#',
+      },
+      {
+        title: '地方自治体',
+        href: '#',
+      },
+      {
+        title: '政党',
+        href: '#',
+      },
+      {
+        title: '政治家',
+        href: '#',
+      },
+      {
+        title: '政治献金',
+        href: '#',
+      },
+      {
+        title: '改革',
+        href: '#',
+      },
+      {
+        title: '民主党',
+        href: '#',
+      },
+      {
+        title: '税制',
+        href: '#',
+      },
+      {
+        title: '安全保障',
+        href: '#',
+      },
+      {
+        title: '外交',
+        href: '#',
+      },
+    ],
+  },
+  {
+    indexText: 'は',
+    lists: [
+      {
+        title: '衆議院',
+        href: '#',
+      },
+      {
+        title: '参議院',
+        href: '#',
+      },
+      {
+        title: '首相',
+        href: '#',
+      },
+      {
+        title: '内閣',
+        href: '#',
+      },
+      {
+        title: '憲法',
+        href: '#',
+      },
+      {
+        title: '自民党',
+        href: '#',
+      },
+      {
+        title: '野党',
+        href: '#',
+      },
+      {
+        title: '総選挙',
+        href: '#',
+      },
+      {
+        title: '議員',
+        href: '#',
+      },
+      {
+        title: '政策',
+        href: '#',
+      },
+      {
+        title: '省庁',
+        href: '#',
+      },
+      {
+        title: '地方自治体',
+        href: '#',
+      },
+      {
+        title: '政党',
+        href: '#',
+      },
+      {
+        title: '政治家',
+        href: '#',
+      },
+      {
+        title: '政治献金',
+        href: '#',
+      },
+      {
+        title: '改革',
+        href: '#',
+      },
+      {
+        title: '民主党',
+        href: '#',
+      },
+      {
+        title: '税制',
+        href: '#',
+      },
+      {
+        title: '安全保障',
+        href: '#',
+      },
+      {
+        title: '外交',
+        href: '#',
+      },
+    ],
+  },
+  {
+    indexText: 'ま',
+    lists: [
+      {
+        title: '衆議院',
+        href: '#',
+      },
+      {
+        title: '参議院',
+        href: '#',
+      },
+      {
+        title: '首相',
+        href: '#',
+      },
+      {
+        title: '内閣',
+        href: '#',
+      },
+      {
+        title: '憲法',
+        href: '#',
+      },
+      {
+        title: '自民党',
+        href: '#',
+      },
+      {
+        title: '野党',
+        href: '#',
+      },
+      {
+        title: '総選挙',
+        href: '#',
+      },
+      {
+        title: '議員',
+        href: '#',
+      },
+      {
+        title: '政策',
+        href: '#',
+      },
+      {
+        title: '省庁',
+        href: '#',
+      },
+      {
+        title: '地方自治体',
+        href: '#',
+      },
+      {
+        title: '政党',
+        href: '#',
+      },
+      {
+        title: '政治家',
+        href: '#',
+      },
+      {
+        title: '政治献金',
+        href: '#',
+      },
+      {
+        title: '改革',
+        href: '#',
+      },
+      {
+        title: '民主党',
+        href: '#',
+      },
+      {
+        title: '税制',
+        href: '#',
+      },
+      {
+        title: '安全保障',
+        href: '#',
+      },
+      {
+        title: '外交',
+        href: '#',
+      },
+    ],
+  },
+  {
+    indexText: 'や',
+    lists: [
+      {
+        title: '衆議院',
+        href: '#',
+      },
+      {
+        title: '参議院',
+        href: '#',
+      },
+      {
+        title: '首相',
+        href: '#',
+      },
+      {
+        title: '内閣',
+        href: '#',
+      },
+      {
+        title: '憲法',
+        href: '#',
+      },
+      {
+        title: '自民党',
+        href: '#',
+      },
+      {
+        title: '野党',
+        href: '#',
+      },
+      {
+        title: '総選挙',
+        href: '#',
+      },
+      {
+        title: '議員',
+        href: '#',
+      },
+      {
+        title: '政策',
+        href: '#',
+      },
+      {
+        title: '省庁',
+        href: '#',
+      },
+      {
+        title: '地方自治体',
+        href: '#',
+      },
+      {
+        title: '政党',
+        href: '#',
+      },
+      {
+        title: '政治家',
+        href: '#',
+      },
+      {
+        title: '政治献金',
+        href: '#',
+      },
+      {
+        title: '改革',
+        href: '#',
+      },
+      {
+        title: '民主党',
+        href: '#',
+      },
+      {
+        title: '税制',
+        href: '#',
+      },
+      {
+        title: '安全保障',
+        href: '#',
+      },
+      {
+        title: '外交',
+        href: '#',
+      },
+    ],
+  },
+  {
+    indexText: 'ら',
+    lists: [
+      {
+        title: '衆議院',
+        href: '#',
+      },
+      {
+        title: '参議院',
+        href: '#',
+      },
+      {
+        title: '首相',
+        href: '#',
+      },
+      {
+        title: '内閣',
+        href: '#',
+      },
+      {
+        title: '憲法',
+        href: '#',
+      },
+      {
+        title: '自民党',
+        href: '#',
+      },
+      {
+        title: '野党',
+        href: '#',
+      },
+      {
+        title: '総選挙',
+        href: '#',
+      },
+      {
+        title: '議員',
+        href: '#',
+      },
+      {
+        title: '政策',
+        href: '#',
+      },
+      {
+        title: '省庁',
+        href: '#',
+      },
+      {
+        title: '地方自治体',
+        href: '#',
+      },
+      {
+        title: '政党',
+        href: '#',
+      },
+      {
+        title: '政治家',
+        href: '#',
+      },
+      {
+        title: '政治献金',
+        href: '#',
+      },
+      {
+        title: '改革',
+        href: '#',
+      },
+      {
+        title: '民主党',
+        href: '#',
+      },
+      {
+        title: '税制',
+        href: '#',
+      },
+      {
+        title: '安全保障',
+        href: '#',
+      },
+      {
+        title: '外交',
+        href: '#',
+      },
+    ],
+  },
+  {
+    indexText: 'わ',
+    lists: [
+      {
+        title: '衆議院',
+        href: '#',
+      },
+      {
+        title: '参議院',
+        href: '#',
+      },
+      {
+        title: '首相',
+        href: '#',
+      },
+      {
+        title: '内閣',
+        href: '#',
+      },
+      {
+        title: '憲法',
+        href: '#',
+      },
+      {
+        title: '自民党',
+        href: '#',
+      },
+      {
+        title: '野党',
+        href: '#',
+      },
+      {
+        title: '総選挙',
+        href: '#',
+      },
+      {
+        title: '議員',
+        href: '#',
+      },
+      {
+        title: '政策',
+        href: '#',
+      },
+      {
+        title: '省庁',
+        href: '#',
+      },
+      {
+        title: '地方自治体',
+        href: '#',
+      },
+      {
+        title: '政党',
+        href: '#',
+      },
+      {
+        title: '政治家',
+        href: '#',
+      },
+      {
+        title: '政治献金',
+        href: '#',
+      },
+      {
+        title: '改革',
+        href: '#',
+      },
+      {
+        title: '民主党',
+        href: '#',
+      },
+      {
+        title: '税制',
+        href: '#',
+      },
+      {
+        title: '安全保障',
+        href: '#',
+      },
+      {
+        title: '外交',
+        href: '#',
+      },
+    ],
+  },
+];
--- src/layouts/docs/docs.tsx
+++ src/layouts/docs/docs.tsx
@@ -1,12 +1,28 @@
 import { FunctionComponent } from 'react';
 import { Header } from '@/components/header';
+import { indexData } from './const';
 import styles from './styles.module.scss';
+import Link from 'next/link';
 
 export const Docs: FunctionComponent = () => {
   return (
     <>
       <Header />
-      <div className={styles.container}>docs</div>
+
+      <ul className={styles.container}>
+        {indexData.map(({ indexText, lists }) => (
+          <li key={indexText}>
+            <p className={styles.aiueo}>{indexText}行</p>
+            <ul>
+              {lists.map(({ title, href }) => (
+                <li key={title}>
+                  <Link href={href}>{title}</Link>
+                </li>
+              ))}
+            </ul>
+          </li>
+        ))}
+      </ul>
     </>
   );
 };
