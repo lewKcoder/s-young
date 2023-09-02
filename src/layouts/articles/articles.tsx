6875ef5002291034590b5db6d56d930a1b969b27
lewKcoder <7kamimura74@gmail.com>
2023-09-03 00:10:15 +0900

    Added header in article layout

--- src/layouts/articles/articles.tsx
+++ src/layouts/articles/articles.tsx
@@ -1,8 +1,15 @@
+import { Header } from '@/components/header';
 import { Component } from './types';
 import styles from './styles.module.scss';
 
 export const Articles: Component = (props) => {
   const { articles } = props;
 
-  return <div className={styles.container}>{articles[0].content}</div>;
+  return (
+    <>
+      <Header hasBlur />
+
+      <div className={styles.container}>{articles[0].content}</div>
+    </>
+  );
 };
